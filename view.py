0570c0e8eb9c9566978943118392b33b2fd24629
ialexofficial <ialexofficial@mail.ru>
2022-02-23 20:58:02 +0300

    Added OOP and divided to modules

---
+++ view.py
@@ -0,0 +1,37 @@
+import entity
+
+
+class View:
+    def __init__(self, filename: str):
+        with open(filename) as file:
+            self.__text = file.read()
+
+        self.__text_processor = entity.TextProcessor(self.__text)
+
+    def start(self):
+        """Start dialog with user."""
+        print(self.__text)
+        print(
+            "Words types count: ",
+            self.__text_processor.count_words_of_type()
+            )
+        print(
+            "Average words count in sentenses: ",
+            self.__text_processor.average_words_count()
+            )
+        print(
+            "Median words count in sentenses: ",
+            self.__text_processor.median_words_count()
+            )
+
+        try:
+            k, n = map(
+                int,
+                input(
+                    "Enter k and n for top anagramms(if don't, left empty): "
+                    ).split()
+            )
+        except ValueError:
+            k, n = 10, 4
+
+        print("Top anagramms: ", self.__text_processor.get_top_ngramms(n, k))
